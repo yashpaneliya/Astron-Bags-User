71514d4b7511c0ce60375bdd02a1716d85995f80
Yash Paneliya <yashsp1158@gmail.com>
2020-07-10 18:33:42 +0530

    Product Model

---
+++ lib/models/productmodel.dart
@@ -0,0 +1,8 @@
+class ProductModel {
+  final model;
+  final imgLink;
+  final category;
+  final gprice;
+  final oprice;
+  ProductModel(this.model, this.imgLink, this.category, this.gprice, this.oprice);
+}
