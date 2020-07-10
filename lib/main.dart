1e03796b8374a61471b15e5f80d023b74f54bcb8
Yash Paneliya <yashsp1158@gmail.com>
2020-07-10 18:15:01 +0530

    Splash screen added

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+Color astronColor = Color.fromRGBO(237, 47, 89, 1);
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashScreen(),
+    theme: ThemeData(fontFamily: 'Raleway', primaryColor: astronColor),
+  ));
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  AnimationController _animationController;
+  Tween _tween;
+  Animation _animation;
+
+  @override
+  void initState() {
+    // mauth = FirebaseAuth.instance;
+    // fetchuser();
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2))
+          ..addListener(() {
+            setState(() {});
+          })
+          ..addStatusListener((status) {
+            setState(() {
+              if (status == AnimationStatus.completed) {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) => HomePage()));
+              }
+            });
+          });
+    _tween = Tween<double>(begin: 30.0, end: 340.0);
+    _animation = _tween.animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut));
+    _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenwidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Container(
+              width: screenwidth * 0.8,
+              height: 400.0,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('images/AstronLogo.png'),
+                      fit: BoxFit.contain)),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 30.0),
+            height: 60.0,
+            alignment: Alignment.center,
+            width: _animation.value,
+            decoration: BoxDecoration(
+              color: astronColor,
+              border: Border.all(width: 1.0, color: astronColor),
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: _animation.value > 300.0
+                ? Text(
+                    'Finished...'.toUpperCase(),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 3.0),
+                  )
+                : Offstage(),
+          ),
+        ],
+      ),
+    );
+  }
+}
